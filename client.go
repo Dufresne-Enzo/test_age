818b8498db80086583dd596cfb686e7624206324
enzo <enzo.dufresne1@viacesi.fr>
2021-02-05 14:23:08 +0100

    add files

---
+++ client.go
@@ -0,0 +1,8 @@
+package client
+
+type client struct {
+    firstName string
+	lastName  string
+	birthdayDate int
+	age int
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "client"
+
+func monAddition(param1 int, param2 int)  int {
+	return param1 + param2
+}
+
+func main()  {
+	var client myClient*
+	myClient = client.newClient(,,,,)
+}
